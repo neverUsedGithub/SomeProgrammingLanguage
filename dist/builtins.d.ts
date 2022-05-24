8bb6da4d3ed3eeb7a74307d85bd6333a892fb7b0
neverUsedGithub <hulyevagyok420@gmail.com>
2022-05-24 16:52:57 +0200

    fixed type, removed .d.ts

--- dist/builtins.d.ts
+++
@@ -1,2 +0,0 @@
-import { Scope } from "./interpreter";
-export default function addTo(scope: Scope): Scope;
--- dist/error.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class Error {
-    static raiseError(input: string, lineno: number, colno: number, errorname: string, errortext: string, length: number): void;
-}
